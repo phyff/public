59e4d022514834756ba35e6b0215fd9c1791bae2
Sequoyah Sudler <33231342+ssudler@users.noreply.github.com>
2021-02-10 10:00:18 -0500

    Update HTTPS enforcement

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const enforce = require('express-sslify');
 
 const app = express();
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+if (process.env.NODE_ENV === 'production') app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 app.use(compression());
 app.use(helmet.contentSecurityPolicy({
