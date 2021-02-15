b865fe2fcd194f705d896a8085783121a2342b8f
rzmay <robertmay@Roberts-iMac.local>
2021-02-15 17:33:11 -0500

    ✍🏾 Updated NoWorkshops wording

--- src/domain/Attend/components/Schedule/NoWorkshops.jsx
+++ src/domain/Attend/components/Schedule/NoWorkshops.jsx
@@ -5,9 +5,9 @@ const NoWorkshops = () => (
   <div>
     <div className="row d-flex flex-column align-items-center justify-content-center">
       <img src={saas2} alt="No Workshops" style={{ width: '35%' }} className="mb-5" />
-      <h3 className="h3">No Workshops Scheduled</h3>
+      <h3 className="h3">Workshops To Be Announced</h3>
       <p className="lead text-center">
-        We currently don't have any workshops scheduled for this year's
+        We're working hard to schedule and confirm workshops for this year's
         event. We're always planning, so check again soon to see if there
         are any workshops that pique your interest!
       </p>
