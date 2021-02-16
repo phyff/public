e81607c8bee7c2bcebf9fe24d0298203737da363
rzmay <robertmay2003@gmail.com>
2021-02-15 20:31:50 -0500

    ⌛️ Updated schedule to show past workshops

---
+++ src/domain/Attend/components/Schedule/ScheduleTable.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Col, ListGroup, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import ScheduleWorkshop from './ScheduleWorkshop';
+import workshopType from '../../../../types/workshop';
+
+const ScheduleTable = ({ workshops, displayTime }) => (
+  <>
+    <Row className="no-gutters d-none d-md-flex bg-white py-3">
+      <Col xl={7} md={6}>
+        <span className="h6 mb-0 text-muted">Workshop</span>
+      </Col>
+      { displayTime && (
+      <Col>
+        <span className="h6 mb-0 text-muted">Time</span>
+      </Col>
+      )}
+      <Col>
+        <span className="h6 mb-0 text-muted">Presenter</span>
+      </Col>
+      <Col className="d-none d-md-block" />
+    </Row>
+    <ListGroup
+      variant="flush"
+    >
+      {workshops
+        .sort((a, b) => (a.time ?? 0) - (b.time ?? 0))
+        .map((workshop, index) => (
+          <ScheduleWorkshop
+            workshop={workshop}
+            delay={index * 50}
+            key={workshop.title}
+            displayTime={displayTime}
+          />
+        ))}
+    </ListGroup>
+  </>
+);
+
+ScheduleTable.propTypes = {
+  workshops: PropTypes.arrayOf(workshopType),
+  displayTime: PropTypes.bool,
+};
+
+export default ScheduleTable;
