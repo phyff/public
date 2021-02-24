cb9def3ecfefa4335dfa64ba68a4f61d1829ac46
ssudler <ssudler21@gmail.com>
2021-02-24 18:07:37 -0500

    Add award winners

--- src/domain/VirtualScreeningRoom/components/AwardWinners.jsx
+++ src/domain/VirtualScreeningRoom/components/AwardWinners.jsx
@@ -3,6 +3,7 @@ import withAuth from '../../../hocs/withAuth';
 import screeningRoomHeader from '../../../data/screeningRoomHeader';
 import Header from '../../../components/Header';
 import withBaseLayout from '../../../hocs/withBaseLayout';
+import IFrame from '../../../components/IFrame';
 
 const AwardWinners = function AwardWinners() {
   return (
@@ -14,7 +15,7 @@ const AwardWinners = function AwardWinners() {
             <div className="col-xl-10">
               <h2 className="h1 mb-4">Award Winners</h2>
               <hr />
-              {/*<IFrame title="Award Winners" src="https://vimeo.com/showcase/8172488/embed" /> */}
+              <IFrame title="Award Winners" src="https://vimeo.com/showcase/8178137/embed" />
             </div>
           </div>
         </div>
