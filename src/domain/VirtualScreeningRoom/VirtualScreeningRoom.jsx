96733322c9cab85b928766d8ee4c459ba57678d1
ssudler <ssudler21@gmail.com>
2021-02-24 08:15:07 -0500

    Fix: Style iframes

--- src/domain/VirtualScreeningRoom/VirtualScreeningRoom.jsx
+++ src/domain/VirtualScreeningRoom/VirtualScreeningRoom.jsx
@@ -9,19 +9,27 @@ const VirtualScreeningRoom = function VirtualScreeningRoom() {
   return (
     <>
       <Header data={mediaHeader} />
-      <section className="p-5">
-        {/*<IFrame title="Reels" src="https://vimeo.com/showcase/8172488/embed" />*/}
-        <p className="h3">Narrative Shorts</p>
-        <IFrame title="Narrative Shorts" src="https://vimeo.com/showcase/8172488/embed" />
-        <p className="h3">Experimental Films</p>
-        <IFrame title="Experimental Films" src="https://vimeo.com/showcase/8172476/embed" />
-        <p className="h3">Documentary Films</p>
-        <IFrame title="Documentary Films" src="https://vimeo.com/showcase/8172471/embed" />
-        <p className="h3">Special Awards</p>
-        <IFrame title="Special Awards" src="https://vimeo.com/showcase/8172466/embed" />
-        <p className="h3">Local Selections</p>
-        <IFrame title="Local Selections" src="https://vimeo.com/showcase/8172456/embed" />
-      </section>
+      {/*<IFrame title="Reels" src="https://vimeo.com/showcase/8172488/embed" />*/}
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-xl-10">
+              <h2 className="h1 mb-4">Selected Films</h2>
+              <hr />
+              <p className="h3">Narrative Shorts</p>
+              <IFrame title="Narrative Shorts" src="https://vimeo.com/showcase/8172488/embed" />
+              <p className="h3">Experimental Films</p>
+              <IFrame title="Experimental Films" src="https://vimeo.com/showcase/8172476/embed" />
+              <p className="h3">Documentary Films</p>
+              <IFrame title="Documentary Films" src="https://vimeo.com/showcase/8172471/embed" />
+              <p className="h3">Special Awards</p>
+              <IFrame title="Special Awards" src="https://vimeo.com/showcase/8172466/embed" />
+              <p className="h3">Local Selections</p>
+              <IFrame title="Local Selections" src="https://vimeo.com/showcase/8172456/embed" />
+            </div>
+          </div>
+        </div>
+    </section>
     </>
   );
 };
