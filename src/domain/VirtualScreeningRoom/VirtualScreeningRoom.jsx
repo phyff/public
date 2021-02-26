97861120d65944ef76c85c3ef1dc0567c3f35317
ssudler <ssudler21@gmail.com>
2021-02-26 00:21:25 -0500

    Remove reels

--- src/domain/VirtualScreeningRoom/VirtualScreeningRoom.jsx
+++ src/domain/VirtualScreeningRoom/VirtualScreeningRoom.jsx
@@ -6,7 +6,6 @@ import Header from '../../components/Header';
 import withBaseLayout from '../../hocs/withBaseLayout';
 import Svg from '../../components/Svg';
 import screeningRoomItems from '../../data/screeningRoomItems';
-import IFrame from '../../components/IFrame';
 
 const VirtualScreeningRoom = function VirtualScreeningRoom() {
   return (
@@ -14,39 +13,39 @@ const VirtualScreeningRoom = function VirtualScreeningRoom() {
       <Header data={screeningRoomHeader} />
       <section>
         <div className="container">
-          <p className="h2">Reels</p>
-          <div className="row">
-            <div className="col-3 align-items-center">
-              <IFrame
-                title="Narrative Reel"
-                src="https://player.vimeo.com/video/516743263?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-                width="640"
-                height="360"
-                allow="autoplay; fullscreen; picture-in-picture"
-                frameBorder="0"
-              />
-            </div>
-            <div className="col-3">
-              <IFrame
-                title="Narrative Reel"
-                src="https://player.vimeo.com/video/516741852"
-                width="640"
-                height="360"
-                allow="autoplay; fullscreen; picture-in-picture"
-                frameBorder="0"
-              />
-            </div>
-            <div className="col-3">
-              <IFrame
-                title="Narrative Reel"
-                src="https://player.vimeo.com/video/516740705"
-                width="640"
-                height="360"
-                allow="autoplay; fullscreen; picture-in-picture"
-                frameBorder="0"
-              />
-            </div>
-          </div>
+          {/*<p className="h2">Reels</p>*/}
+          {/*<div className="row">*/}
+          {/*  <div className="col-3 align-items-center">*/}
+          {/*    <IFrame*/}
+          {/*      title="Narrative Reel"*/}
+          {/*      src="https://player.vimeo.com/video/516743263?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"*/}
+          {/*      width="640"*/}
+          {/*      height="360"*/}
+          {/*      allow="autoplay; fullscreen; picture-in-picture"*/}
+          {/*      frameBorder="0"*/}
+          {/*    />*/}
+          {/*  </div>*/}
+          {/*  <div className="col-3">*/}
+          {/*    <IFrame*/}
+          {/*      title="Narrative Reel"*/}
+          {/*      src="https://player.vimeo.com/video/516741852"*/}
+          {/*      width="640"*/}
+          {/*      height="360"*/}
+          {/*      allow="autoplay; fullscreen; picture-in-picture"*/}
+          {/*      frameBorder="0"*/}
+          {/*    />*/}
+          {/*  </div>*/}
+          {/*  <div className="col-3">*/}
+          {/*    <IFrame*/}
+          {/*      title="Narrative Reel"*/}
+          {/*      src="https://player.vimeo.com/video/516740705"*/}
+          {/*      width="640"*/}
+          {/*      height="360"*/}
+          {/*      allow="autoplay; fullscreen; picture-in-picture"*/}
+          {/*      frameBorder="0"*/}
+          {/*    />*/}
+          {/*  </div>*/}
+          {/*</div>*/}
           <div className="row justify-content-center">
             <div className="col-lg-10 col-xl-8">
               {screeningRoomItems.map((item) => (
