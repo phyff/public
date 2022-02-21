7e091e4807a4ea747f24a5b0d4728c57516da3e6
Martina Kiewek <martinakiewek@Martinas-MacBook-Air.local>
2022-02-21 15:13:21 -0500

    updated screening room content

--- src/domain/VirtualScreeningRoom/components/Screenplays.jsx
+++ src/domain/VirtualScreeningRoom/components/Screenplays.jsx
@@ -15,71 +15,13 @@ const Screenplays = function Screenplays() {
             <div className="col-xl-10">
               <h2 className="h1 mb-4">Screenplays</h2>
               <hr />
-              <p className="h3">A Day With Payne by Alex Colocousis</p>
-              <IFrame
-                title="A Day with Payne"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=adaywithpaynedraft3&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">A La Mode by Kaya Trefz</p>
-              <IFrame
-                title="A Day with Payne"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=alamodesixthdraft&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">Boy on the Moon by Jake Lubin</p>
-              <IFrame
-                title="A Day with Payne"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=boyonthemoon&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">Don’t Come Near Me by Christian Lee</p>
-              <IFrame
-                title="Don’t Come Near Me"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=dontcomenearme&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">Driving Test by Graham Kohn</p>
-              <IFrame
-                title="Driving Test"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=drivingtest&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">Outgrowing My Shoes by Kaya Trefz</p>
-              <IFrame
-                title="Outgrowing My Shoes"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=outgrowingmyshoes&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">Ready, Set, Go! by Grace Raufer</p>
-              <IFrame
-                title="Ready, Set, Go!"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=readysetgofinal&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">Sandwich Heist by Brody Bundis</p>
-              <IFrame
-                title="Sandwich Heist"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=sandwichheist2&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">School Of Theft by Robert Madden</p>
-              <IFrame
-                title="School Of Theft"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=schooloftheftofficialscreenplay&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">The Interview by Sean Park</p>
-              <IFrame
-                title="The Interview"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=scripttheinterview3&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">The Bonus Baby by Feeney Lan</p>
-              <IFrame
-                title="The Bonus Baby"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=thebonusbaby&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">The Roommate by Jake Lubin</p>
-              <IFrame
-                title="The Roommate"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=theroommatev2&hideIssuuLogo=true&u=germantownfds"
-              />
-              <p className="h3">We All Make The Little Flowers Grow by Kaya L. Trefz</p>
-              <IFrame
-                title="We All Make The Little Flowers Grow"
-                src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=weallmakethelittleflowersgrow&hideIssuuLogo=true&u=germantownfds"
-              />
+              <p className="h3"><a href="https://issuu.com/phillyfilmfest/docs/_don_t_let_sleeping_dogs_lie.pdfscript">(Don't) Let Sleeping Dogs Lie by Laura Atanasov </a></p>
+              {/*<IFrame*/}
+              {/*  title="A Day with Payne"*/}
+              {/*  src="//e.issuu.com/embed.html?backgroundColor=%23bbd6d8&backgroundColorFullscreen=%23bbd6d8&d=adaywithpaynedraft3&hideIssuuLogo=true&u=germantownfds"*/}
+              {/*/>*/}
+              <p className="h3"><a href="https://issuu.com/phillyfilmfest/docs/kofichroniclespilot">Kofi Chronicles by Kashvi Ramani </a></p>
+              <p className="h3"><a href="https://issuu.com/phillyfilmfest/docs/bottledup">Bottled Up by Lex Jimenez </a></p>
             </div>
           </div>
         </div>
