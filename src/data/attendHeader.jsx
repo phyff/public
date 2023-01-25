42f6cc56d7b5f2b56ded33a626687b5e4efce4ec
Sequoyah Sudler <33231342+ssudler@users.noreply.github.com>
2023-01-25 01:09:48 -0800

    Edit copy
    Signed-off-by: Sequoyah Sudler <33231342+ssudler@users.noreply.github.com>

--- src/data/attendHeader.jsx
+++ src/data/attendHeader.jsx
@@ -8,18 +8,18 @@ const attendHeader = {
   dividerColor: 'bg-primary-3',
   caption: () => (
     <div>
-      Join us for the fifth annual Philadelphia Youth Film Festival!
+      Join us for the sixth annual Philadelphia Youth Film Festival, hosted on Saturday, February 25, 2023 from 11am to 6pm EST.
+      We're pleased to announce that Matt Grady will be serving as this year's keynote speaker. Mr. Grady is a producer, actor, and editor from New York City, as well as the founder of Factory 25, an independent film distribution company housing conceptually provocative narratives and documentaries. His mission is to expose the world to under-the-radar films, music, and other curiosities theatrically and digitally. In 2014, Mr. Grady was named "One of the Most Influential People in Brooklyn Culture” and has been called "one of the most important curatorial voices of new, independent American films." We are excited to have him speak at our festival!
+
+      <strong>FILMS!</strong> - You'll receive a link the week prior to the festival that will enable you to screen the entire collection of this year's selected films. In addition, winning films will be screened in person at our event in Philadelphia on February 25.
       <br /><br />
-      <strong>- FILMS!  </strong> - You'll receive a link the week prior to the festival that will enable you to screen the entire collection of this year's selected films.
+      <strong>KEYNOTE SPEAKER</strong> - Join us for our keynote presentation with producer, actor, and editor MATT GRADY.
       <br /><br />
-      <strong>- KEYNOTE SPEAKER! </strong>- You'll receive the link to the webinar to join us for our keynote presentation with actor AMANDA PEET!
+      <strong>STUDENT FILMMAKER PANEL</strong> - Join us for a Q&A session with some of our finalists from around the world. If you cannot join us in person, you’ll receive a link to join the panel virtually.
       <br /><br />
-      <strong>- FILMMAKER TALKBACK SESSION! </strong>- You'll receive the link to join us for a virtual Q&A with some of our finalists.
+      <strong>WORKSHOPS</strong> - Participate in workshops with film industry professionals,covering a wide variety of general and niche filmmaking topics. Whether you want to learn about how films are made or are interested in making your own, we'd love to see you at our workshops.
       <br /><br />
-      This free event is open to all high school students.
-      The Festival is hosted by Germantown Friends School and will take place on Saturday, February 25, 2023.
-      We will contact you closer to the event with a confirmation of exact start times and links to the webinar,
-      zoom and any additional information and updates on ways that you can participate in the event!
+      This free event is open to all high school students (Students must be between the ages of 14 and 19 and should currently be enrolled in high school, or have graduated from high school in 2022). The Festival is hosted by Germantown Friends School and will take place on Saturday, February 25, 2023. If you are unable to join us in person for the festival, we would still like to invite you to view the films online and participate virtually in the Student Filmmaker Panel on the day of the event.
     </div>
   ),
   decorationStyle: 1,
