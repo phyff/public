4e94435d5378209dfe67768e658cbbb3b11ec9c2
crunchy-nutella <94563161+crunchy-nutella@users.noreply.github.com>
2023-02-19 21:35:18 -0500

    Update AwardWinners.jsx

--- src/domain/VirtualScreeningRoom/components/AwardWinners.jsx
+++ src/domain/VirtualScreeningRoom/components/AwardWinners.jsx
@@ -15,7 +15,7 @@ const AwardWinners = function AwardWinners() {
             <div className="col-xl-10">
               <h2 className="h1 mb-4">Award Winners</h2>
               <hr />
-              <iframe src='https://vimeo.com/showcase/10167149/embed' allowfullscreen frameborder='0' style='position:absolute;top:0;left:0;width:100%;height:100%;'></iframe>
+              <iframe src='https://vimeo.com/showcase/10167149/embed' allowfullscreen frameborder='0'></iframe>
             </div>
           </div>
         </div>
