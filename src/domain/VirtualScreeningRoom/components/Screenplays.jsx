1fafe2a050d326eb4f973c054fc93645f311fbbe
crunchy-nutella <94563161+crunchy-nutella@users.noreply.github.com>
2023-02-20 19:18:57 -0500

    Update Screenplays.jsx

--- src/domain/VirtualScreeningRoom/components/Screenplays.jsx
+++ src/domain/VirtualScreeningRoom/components/Screenplays.jsx
@@ -16,15 +16,15 @@ const Screenplays = function Screenplays() {
               <h2 className="h1 mb-4">Screenplays</h2>
               <hr />
               <p className="h3">Marathon</p>
-              <IFrame title = "Marathon" src="https://e.issuu.com/embed.html?d=2marathon_script&hideIssuuLogo=true&u=germantownfds">
+              <IFrame title = "Marathon" src="https://e.issuu.com/embed.html?d=2marathon_script&hideIssuuLogo=true&u=germantownfds"/>
               <p className="h3">A Little Bit of Trouble Making</p>
-              <IFrame title = "A Little Bit of Trouble Making" src="https://e.issuu.com/embed.html?d=1alittlebitoftroublemakingscript&hideIssuuLogo=true&u=germantownfds"></iframe>
+              <IFrame title = "A Little Bit of Trouble Making" src="https://e.issuu.com/embed.html?d=1alittlebitoftroublemakingscript&hideIssuuLogo=true&u=germantownfds"/>
               <p className="h3">Solicitude</p>
-              <IFrame title = "Solicitude" src="https://e.issuu.com/embed.html?d=3scriptsolicitude2&hideIssuuLogo=true&u=germantownfds">
+              <IFrame title = "Solicitude" src="https://e.issuu.com/embed.html?d=3scriptsolicitude2&hideIssuuLogo=true&u=germantownfds"/>
               <p className="h3">The Death of a Moth</p>
-              <IFrame title = "The Death of a Moth" src="https://e.issuu.com/embed.html?d=4scriptthedeathofamoth21&hideIssuuLogo=true&u=germantownfds">
+              <IFrame title = "The Death of a Moth" src="https://e.issuu.com/embed.html?d=4scriptthedeathofamoth21&hideIssuuLogo=true&u=germantownfds"/>
               <p className="h3">Sole Searching</p>
-              <IFrame title = "Sole Searching" src="https://e.issuu.com/embed.html?d=6solesearching_script&hideIssuuLogo=true&u=germantownfds">
+              <IFrame title = "Sole Searching" src="https://e.issuu.com/embed.html?d=6solesearching_script&hideIssuuLogo=true&u=germantownfds"/>
               <p className="h3">Silent Night</p>
               <IFrame title="Silent Night" src="https://e.issuu.com/embed.html?d=5silentnight_script&hideIssuuLogo=true&u=germantownfds" />
               <p className="h3">Sunflowers</p>
